2e189266933ab5a87346c30e912c137f65c2bdce
leandrocgsi <leandrocgsi@gmail.com>
2024-12-19 05:51:33 -0300

    REST Parameters and Exception Handler

--- 05_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/GreetingController.java
+++ 05_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/GreetingController.java
@@ -1,7 +1,9 @@
 package br.com.erudio.controllers;
 
 import br.com.erudio.model.Greeting;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -9,13 +11,13 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-
     private final AtomicLong counter = new AtomicLong();
 
-    // @RequestMapping("/greeting")
-    // @RequestMapping(method = RequestMethod.DELETE, name = "/greeting")
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    // http://localhost:8080/greeting?name=Leandro
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "Word")
+            String name){
+        return  new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
