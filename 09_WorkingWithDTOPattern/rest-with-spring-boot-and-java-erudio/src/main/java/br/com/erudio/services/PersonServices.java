8b8e6c78c83cef86f1f647adf71466151924ba56
leandrocgsi <leandrocgsi@gmail.com>
2024-12-20 10:54:17 -0300

    Implementing DTO pattern with Dozer

---
+++ 09_WorkingWithDTOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,67 @@
+package br.com.erudio.services;
+
+import br.com.erudio.exception.ResourceNotFoundException;
+import br.com.erudio.model.Person;
+import br.com.erudio.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
+
+
+    public List<Person> findAll() {
+
+        logger.info("Finding all People!");
+
+        return repository.findAll();
+    }
+
+    public Person findById(Long id) {
+        logger.info("Finding one Person!");
+
+        return repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+    }
+
+    public Person create(Person person) {
+
+        logger.info("Creating one Person!");
+
+        return repository.save(person);
+    }
+
+    public Person update(Person person) {
+
+        logger.info("Updating one Person!");
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return repository.save(person);
+    }
+
+    public void delete(Long id) {
+
+        logger.info("Deleting one Person!");
+
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        repository.delete(entity);
+    }
+}
