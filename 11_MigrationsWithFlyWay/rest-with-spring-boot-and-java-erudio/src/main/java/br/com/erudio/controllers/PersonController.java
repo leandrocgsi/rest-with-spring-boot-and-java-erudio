6f7f58b994ac946c699e95d0cd782284ecf41ef4
leandrocgsi <leandrocgsi@gmail.com>
2024-12-21 12:18:49 -0300

    Adding migrations support with Flyway

--- 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
     @Autowired
