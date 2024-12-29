f3b514bd41da6ceae9d192687651c4cfcb7e9ddd
leandrocgsi <leandrocgsi@gmail.com>
2024-12-29 18:58:34 -0300

    Implementing Upload and Download Files

--- 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/FileController.java
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/FileController.java
@@ -7,11 +7,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -51,8 +51,26 @@ public class FileController implements FileControllerDocs {
                 .collect(Collectors.toList());
     }
 
+    @GetMapping("/downloadFile/{fileName:.+}")
     @Override
-    public ResponseEntity<ResponseEntity> downloadFile(String fileName, HttpServletRequest request) {
-        return null;
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        Resource resource = service.loadFileAsResource(fileName);
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.error("Could not determine file type!");
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+            .contentType(MediaType.parseMediaType(contentType))
+            .header(
+                HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + resource.getFilename() + "\"")
+            .body(resource);
     }
 }
--- 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/docs/FileControllerDocs.java
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/docs/FileControllerDocs.java
@@ -3,6 +3,7 @@ package br.com.erudio.controllers.docs;
 import br.com.erudio.data.dto.UploadFileResponseDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +14,6 @@ public interface FileControllerDocs {
 
     UploadFileResponseDTO uploadFile(MultipartFile file);
     List<UploadFileResponseDTO> uploadMultipleFiles(MultipartFile[] files);
-    ResponseEntity<ResponseEntity> downloadFile(String fileName,
-                                                HttpServletRequest request);
+    ResponseEntity<Resource> downloadFile(String fileName,
+                                          HttpServletRequest request);
 }
