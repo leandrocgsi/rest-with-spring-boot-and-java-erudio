e1d273e15c1f76db567ced0f3d53990bf8324564
leandrocgsi <leandrocgsi@gmail.com>
2024-12-29 15:29:37 -0300

    Implementing Upload and Download Files

---
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -0,0 +1,49 @@
+package br.com.erudio.services;
+
+import br.com.erudio.config.FileStorageConfig;
+import br.com.erudio.exception.FileStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath()
+                .toAbsolutePath().normalize();
+
+        this.fileStorageLocation = path;
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where files will be stored!", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename Contains a Invalid path Sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try Again!", e);
+        }
+    }
+}
