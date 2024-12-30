cc169e3df4698a3e7ae50d2a269c4f9bf4b50380
leandrocgsi <leandrocgsi@gmail.com>
2024-12-29 22:56:33 -0300

    Docker Overview Session - After Dockerization

--- 16_IntroToConteinersWithDocker/02-hello-docker-after/src/main/java/br/com/erudio/model/HelloDocker.java
+++ 16_IntroToConteinersWithDocker/02-hello-docker-after/src/main/java/br/com/erudio/model/HelloDocker.java
@@ -1,20 +1,3 @@
 package br.com.erudio.model;
 
-public class HelloDocker {
-	
-	private final String content;
-	private final String environment;
-	
-	public HelloDocker(String content, String environment) {
-		this.content = content;
-		this.environment = environment;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public String getEnvironment() {
-		return environment;
-	}
-}
+public record HelloDocker(String content, String environment) {}
