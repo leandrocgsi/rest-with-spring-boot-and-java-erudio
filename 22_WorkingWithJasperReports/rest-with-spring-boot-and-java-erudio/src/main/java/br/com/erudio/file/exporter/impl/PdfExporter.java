92587956ce3b5ee5bba4fe8a279aa826fc8685cc
leandrocgsi <leandrocgsi@gmail.com>
2025-01-02 02:57:49 -0300

    Generating Professional Reports with JasperReports and Spring Boot

--- 22_WorkingWithJasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/exporter/impl/PdfExporter.java
+++ 22_WorkingWithJasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/exporter/impl/PdfExporter.java
@@ -2,16 +2,38 @@ package br.com.erudio.file.exporter.impl;
 
 import br.com.erudio.data.dto.PersonDTO;
 import br.com.erudio.file.exporter.contract.FileExporter;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class PdfExporter implements FileExporter {
 
     @Override
     public Resource exportFile(List<PersonDTO> people) throws Exception {
-        return null;
+        InputStream inputStream = getClass().getResourceAsStream("/templates/people.jrxml");
+        if (inputStream == null) {
+            throw new RuntimeException("Template file not found: /templates/people.jrxml");
+        }
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(people);
+        Map<String, Object> parameters = new HashMap<>();
+        // parameters.put("title", "People Report");
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
+            JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+            return new ByteArrayResource(outputStream.toByteArray());
+        }
     }
 }
