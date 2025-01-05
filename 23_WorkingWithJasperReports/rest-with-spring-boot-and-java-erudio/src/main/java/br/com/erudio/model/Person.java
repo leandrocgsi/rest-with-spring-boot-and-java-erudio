fd7f89f22519b70f1932c017ec8854b0a2174427
leandrocgsi <leandrocgsi@gmail.com>
2025-01-04 22:51:19 -0300

    Generating Professional Reports with JasperReports and Spring Boot

--- 23_WorkingWithJasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
+++ 23_WorkingWithJasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
@@ -40,8 +40,8 @@ public class Person implements Serializable {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
          name = "person_books",
-         joinColumns = @JoinColumn(name = "book_id"),
-         inverseJoinColumns = @JoinColumn(name = "person_id")
+         joinColumns = @JoinColumn(name = "person_id"),
+         inverseJoinColumns = @JoinColumn(name = "book_id")
     )
     private List<Book> books;
 
