58cf5c44491b7fd23d181060cb350219727610ef
leandrocgsi <leandrocgsi@gmail.com>
2025-01-07 15:07:26 -0300

    Implementing Authentication with Spring Security and JWT

---
+++ 25_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.dto.security.AccountCredentialsDTO;
+import br.com.erudio.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Authentication Endpoint!")
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    AuthService service;
+
+    @Operation(summary = "Authenticates an user and returns a token")
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody AccountCredentialsDTO credentials) {
+        if (credentialsIsInvalid(credentials))return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        var token = service.signIn(credentials);
+
+        if (token == null) ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        return  ResponseEntity.ok().body(token);
+    }
+
+    private static boolean credentialsIsInvalid(AccountCredentialsDTO credentials) {
+        return credentials == null ||
+                StringUtils.isBlank(credentials.getPassword()) ||
+                StringUtils.isBlank(credentials.getUsername());
+    }
+}
