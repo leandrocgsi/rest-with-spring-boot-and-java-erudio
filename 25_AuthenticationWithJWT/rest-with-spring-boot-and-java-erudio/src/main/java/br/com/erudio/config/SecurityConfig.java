c3d65598723886d021bb3b2ec755f778f7fbf50b
leandrocgsi <leandrocgsi@gmail.com>
2025-01-07 12:43:07 -0300

    Implementing Authentication with Spring Security and JWT

---
+++ 25_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package br.com.erudio.config;
+
+import br.com.erudio.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    public SecurityConfig(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+
+        PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
+                "", 8, 185000,
+                Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("pbkdf2", pbkdf2Encoder);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+        return passwordEncoder;
+    }
+}
