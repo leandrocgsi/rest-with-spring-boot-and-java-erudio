7bf9d591709c467163dc4d940ece632f44c25af7
leandrocgsi <leandrocgsi@gmail.com>
2025-01-09 12:23:59 -0300

    Shielding our API with Rest Assured, Testcontainers and JUnit 5

--- 26_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/AuthController.java
+++ 26_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/AuthController.java
@@ -28,7 +28,7 @@ public class AuthController implements AuthControllerDocs {
         var token = service.signIn(credentials);
 
         if (token == null) ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
-        return  ResponseEntity.ok().body(token);
+        return token;
     }
 
     @PutMapping("/refresh/{username}")
@@ -39,7 +39,7 @@ public class AuthController implements AuthControllerDocs {
         if (parametersAreInvalid(username, refreshToken)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
         var token = service.refreshToken(username, refreshToken);
         if (token == null) ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
-        return  ResponseEntity.ok().body(token);
+        return token;
     }
 
     @PostMapping(value = "/createUser",
