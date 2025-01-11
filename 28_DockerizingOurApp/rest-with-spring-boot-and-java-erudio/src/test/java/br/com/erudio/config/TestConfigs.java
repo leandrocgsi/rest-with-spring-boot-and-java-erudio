18045e615da74da5d1d1aa4e8595ea331ce994fc
leandrocgsi <leandrocgsi@gmail.com>
2025-01-11 07:30:56 -0300

    Dockerizing our RESTfull API Application

--- 28_DockerizingOurApp/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/config/TestConfigs.java
+++ 28_DockerizingOurApp/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/config/TestConfigs.java
@@ -1,7 +1,7 @@
 package br.com.erudio.config;
 
 public interface TestConfigs {
-    int SERVER_PORT = 80;
+    int SERVER_PORT = 8888;
 
     String HEADER_PARAM_AUTHORIZATION = "Authorization";
     String HEADER_PARAM_ORIGIN = "Origin";
