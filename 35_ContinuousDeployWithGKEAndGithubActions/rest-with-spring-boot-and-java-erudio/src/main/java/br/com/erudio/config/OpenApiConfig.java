7471b29bea4cdae10407d8d4eee774e871e4aa6d
leandrocgsi <leandrocgsi@gmail.com>
2025-01-14 15:02:01 -0300

    Continuous Deployment with Google Kubernetes Engine (GKE) and Github Actions

--- 35_ContinuousDeployWithGKEAndGithubActions/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ 35_ContinuousDeployWithGKEAndGithubActions/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -1,19 +1,24 @@
 package br.com.erudio.config;
 
+import br.com.erudio.services.InstanceInformationService;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Autowired
+    InstanceInformationService service;
+
     @Bean
     OpenAPI customOpenAPI(){
         return new OpenAPI()
             .info(new Info()
-                .title("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
+                .title("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker V1 " + service.retrieveInstanceInfo())
                     .version("v1")
                     .description("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
                     .termsOfService("https://pub.erudio.com.br/meus-cursos")
